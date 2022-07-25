38418bcca25dde899bd38e7221d1c5324b916b7a
shivani <79415472+shivanisingh03@users.noreply.github.com>
2022-07-25 20:08:49 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,11 @@
+dic={"a":4,"b":2,"c":3,"d":4}
+max=0
+for i in dic:
+    if max<dic[i]:
+        max=dic[i]
+print({i:max})
+
+
+
+
+
---
+++ min.py
@@ -0,0 +1,12 @@
+dic={"a":4,"b":2,"c":3,"d":4}
+min=dic["a"]
+for i in dic:
+    key=dic[i]
+    if key<min:
+        min=key
+print({i:min})
+
+
+
+
+
